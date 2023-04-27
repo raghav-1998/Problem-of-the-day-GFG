67d0d1f8d6736088ccbf60bb716e284b3c893056
raghav-1998 <raghav.vasdeva@gmail.com>
2023-04-27 23:50:37 +0530

    GFG 19th April POTD

---
+++ Wifi Range/Wifi-Range.py
@@ -0,0 +1,24 @@
+#User function Template for python3
+class Solution:
+    def wifiRange(self, N, S, X): 
+        #code here
+        st=0
+        while st<N and S[st]!='1':
+            st+=1
+
+
+        if(st==N or st>X):
+            return False
+
+        last=st
+        for i in range(st,N):
+            if((i-last+1)>2*(X+1)):
+                #print("{} and {}".format(last,i))
+                return False
+            if(S[i]=='1'):
+                last=i
+        
+        if(N-last>(X+1)):
+            return False
+            
+        return True
