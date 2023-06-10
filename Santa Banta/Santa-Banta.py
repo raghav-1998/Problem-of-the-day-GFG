b9bb9949f5fa58cdab986fa292fb4b57002391cd
raghav-1998 <raghav.vasdeva@gmail.com>
2023-06-10 21:23:02 +0530

    GFG 2nd June POTD

---
+++ Santa Banta/Santa-Banta.py
@@ -0,0 +1,44 @@
+#User function Template for python3
+
+class Solution:
+    def precompute(self):
+        primes = [True]*(10**6)
+        primes[0] = False
+        primes[1] = False
+        for i in range(len(primes)):
+            if not primes[i]:
+                continue
+            for j in range(i*i, len(primes), i):
+                primes[j] = False
+        self.primes = [i for i, t in enumerate(primes) if t]
+        
+    
+    def helpSanta (self, n, m, g) : 
+        from collections import defaultdict
+        if m == 0:
+            return -1
+            
+        arr = [i for i in range(n+1)]
+        
+        def find(i):
+            if arr[i] != i:
+                arr[i] = find(arr[i])
+            return arr[i]
+            
+        for i, grp in enumerate(g):
+            if i > n:
+                break
+            
+            rx = find(i)
+            for y in grp:
+                ry = find(y)
+                if rx != ry:
+                    arr[ry] = rx
+                
+        num = 0
+        cnt = defaultdict(int)
+        for i in range(1, n+1):
+            r = find(i)
+            cnt[r] += 1
+            num = max(num, cnt[r])
+        return self.primes[num-1]
