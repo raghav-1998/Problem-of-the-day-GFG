c8353ea4be083482a1cd7d97a3d0df336dc2a9c2
raghav-1998 <raghav.vasdeva@gmail.com>
2023-06-28 23:57:38 +0530

    GFG 19th June POTD

---
+++ Rearrange an array with O(1) extra space/Rearrange-an-array-with-O(1)-extra-space.py
@@ -0,0 +1,16 @@
+#User function Template for python3
+
+##Complete this code
+
+class Solution:
+    #Function to rearrange an array so that arr[i] becomes arr[arr[i]]
+    #with O(1) extra space.
+    def arrange(self,arr, n): 
+        #Your code here
+        for i in range(n):
+            arr[i] = arr[i] + (arr[arr[i]]%n)*n
+        
+        for i in range(n):
+            arr[i] = arr[i]//n
+        
+        return 
